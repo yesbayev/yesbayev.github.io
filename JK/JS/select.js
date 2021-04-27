a58d16883018148d0abece061fe0a4b3185366b7
yesbayev <78971429+yesbayev@users.noreply.github.com>
2021-04-27 15:28:20 +0600

    Delete JK directory

--- JK/JS/select.js
+++
@@ -1,7 +0,0 @@
-//  Select
-document.querySelector('.building-select').addEventListener('click', () => {
-    let data = document.querySelector('.building-select').value;
-    document.querySelector('.out-10').innerHTML = data;
-    // document.querySelector('.s-10').value = 'winamp';
-});
-//  Select
